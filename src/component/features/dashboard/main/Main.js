8db708ec3f1fbc0ff1d5941532c6ad0d62c318fc
Syoudai Shiraishi <55680231+k18rs057@users.noreply.github.com>
2022-10-20 13:00:18 +0900

    done task 3

--- src/component/features/dashboard/main/Main.js
+++ src/component/features/dashboard/main/Main.js
@@ -2,6 +2,9 @@ import React from 'react'
 
 export default function Main() {
   return (
-    <div>Change source code from GR-1</div>
+    <>
+      <div>Change source code from GR-1</div>
+      <p>done task 4</p>
+    </>
   )
 }
