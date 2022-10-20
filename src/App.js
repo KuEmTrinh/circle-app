fa730fd6ce91c14925acf1b366d43023b3889622
KuEmTrinh <91253322+KuEmTrinh@users.noreply.github.com>
2022-10-20 12:51:44 +0900

    私Dashboard追加しました

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import "./App.css";
 import "./Reset.css";
 import { Routes, Route } from "react-router-dom";
 import Main from "./component/features/app/main/Main";
+import MainDashboard from "./component/features/dashboard/main/MainDashboard";
 function App() {
   return (
     <div className="app">
       <Routes>
         <Route path="/" element={<Main />} />
+        <Route path="/dashboard" element={<MainDashboard />} />
       </Routes>
     </div>
   );
