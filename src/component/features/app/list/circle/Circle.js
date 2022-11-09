cd8028f9eaab1341f6508915944ccebfe6b683e7
phamhieu99hd@gmail.com <phamhieu99hd@gmail.com>
2022-11-09 21:55:10 +0900

    get data and import to circle homepage -done

--- src/component/features/app/list/circle/Circle.js
+++ src/component/features/app/list/circle/Circle.js
@@ -1,19 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
+import CarouselSlide from "../../../../ui/CarouselSlide";
 import { db } from "./../../../../../app/firebase";
 
-function CircleHomePage({ circleId }) {
- console.log(circleId)
- const circleInfor =db.collection("circle").doc(circleId)
 
-console.log(circleInfor)
+function CircleHomePage({ circleId }) {
+  const [dataCircleInfor, setDataCircleInfor] = useState({})
+  const circleInforFromFirebase = async () => db.collection("circle").doc(circleId).get().then((doc) => {
+    const dataInfor = {
+      name: doc.data().name,
+      type: doc.data().type
+    }
+    setDataCircleInfor(dataInfor)
+  })
+  circleInforFromFirebase();
+
+  return <>
+    <div>
+      <h1 className="circleName">{dataCircleInfor.name}</h1>
+      <div>
+        <CarouselSlide/>
+      </div>
+    </div>
+  </>
+
 }
 export default function Circle() {
   let { circleId } = useParams();
   return (
     <>
-    {circleId?<CircleHomePage circleId={circleId}></CircleHomePage>:""}
-      
+      <CircleHomePage circleId={circleId}></CircleHomePage>
+
     </>
   );
 }
---
+++ src/component/ui/CarouselSlide.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Carousel from 'react-bootstrap/Carousel'
+export default function CarouselSlide() {
+    return (
+        <>
+            
+
+        </>
+    )
+}
