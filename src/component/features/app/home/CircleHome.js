1d09cea774eb12089eec0b27a5f1fe2b7889b954
PMH8 <phamhieu99hd@gmail.com>
2022-11-15 15:43:14 +0900

    edit myCircleHompage-notDone

--- src/component/features/app/home/CircleHome.js
+++ src/component/features/app/home/CircleHome.js
@@ -3,16 +3,17 @@ import CircleMenu from "../../../ui/CircleMenu";
 import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
 import ButtonComponent from "../../../ui/ButtonComponent";
+import "./CircleHome.css"
 export default function CircleHome() {
   let { circleId } = useParams();
   return (
-    <>
+    <div className="circleHome">
       <CircleMenu></CircleMenu>
       <div className="homeTitle">
-        <Link to={"/chat/" + circleId}>
+        {/* <Link to={"/chat/" + circleId}>
           <ButtonComponent>Click to Chat</ButtonComponent>
-        </Link>
+        </Link> */}
       </div>
-    </>
+    </div>
   );
 }
