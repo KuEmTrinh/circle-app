0cddd738b9b6412717ede64389dd85c029bccf21
KuEmTrinh <91253322+KuEmTrinh@users.noreply.github.com>
2022-11-24 12:00:21 +0900

    update

--- src/component/features/app/home/members/Member.js
+++ src/component/features/app/home/members/Member.js
@@ -1,57 +1,187 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import FiberManualRecordIcon from "@mui/icons-material/FiberManualRecord";
-import DangerousIcon from "@mui/icons-material/Dangerous";
-import CheckBoxIcon from "@mui/icons-material/CheckBox";
+import CancelIcon from "@mui/icons-material/Cancel";
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
-import { red, green } from "@mui/material/colors";
+import Button from "@mui/material/Button";
+import { db } from "../../../../../app/firebase";
 import "./Members.css";
-export default function Members() {
+function MemberListComponent({ memberList }) {
+  const [circleMembers, setCircleMembers] = useState();
+  useEffect(() => {
+    const regesteringMemberList = [...memberList];
+    let filterList = regesteringMemberList.filter((el) => el.status == true);
+    setCircleMembers(filterList);
+  }, [memberList]);
+  return (
+    <div className="memberListBox">
+      <div className="circleTitleBox">
+        {<FiberManualRecordIcon fontSize="small" />}
+        <p>メンバーリスト</p>
+      </div>
+      <div className="memberList">
+        {circleMembers ? (
+          <>
+            {circleMembers.map((el) => {
+              return (
+                <MemberCardComponent
+                  member={el}
+                  key={el.id}
+                ></MemberCardComponent>
+              );
+            })}
+          </>
+        ) : (
+          "Loading"
+        )}
+      </div>
+    </div>
+  );
+}
+
+function MemberCardComponent({ member }) {
+  return (
+    <div className="memberCard flex">
+      <div className="memberCardAvatar">
+        <img src={member.userPhotoURL} />
+      </div>
+      <div className="memberCardInfo">
+        <p className="memberCardUsername">{member.userName}</p>
+        <p className="memberCardUsercode">{member.userNumber}</p>
+        <div className="memberCardTag">
+          <p className="memberCardUserrole">{member.role == "circleAdmin" ? '会長': '会員'}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function MemberJoinListComponent({ memberList, circleId }) {
+  const [registerList, setRegisterList] = useState();
+  useEffect(() => {
+    const regesteringMemberList = [...memberList];
+    let filterList = regesteringMemberList.filter((el) => el.status == false);
+    setRegisterList(filterList);
+  }, [memberList]);
+
+  return (
+    <div className="wantToJoinUserBox">
+      <div className="circleTitleBox">
+        {<FiberManualRecordIcon fontSize="small" />}
+        <p>参加したいメンバーリスト</p>
+      </div>
+      {registerList ? (
+        <>
+          {registerList.map((el) => {
+            return (
+              <MemberJoinComponent
+                registerMember={el}
+                key={el.id}
+                circleId={circleId}
+              ></MemberJoinComponent>
+            );
+          })}
+        </>
+      ) : (
+        ""
+      )}
+    </div>
+  );
+}
+
+function MemberJoinComponent({ registerMember, circleId }) {
+  const registerMemberCancel = () => {
+    const query = db
+      .collection("circle")
+      .doc(circleId)
+      .collection("member")
+      .doc(registerMember.id)
+      .delete();
+    return query;
+  };
+  const registerMemberAccept = () => {
+    const query = db
+      .collection("circle")
+      .doc(circleId)
+      .collection("member")
+      .doc(registerMember.id)
+      .update({
+        status: true,
+      });
+    return query;
+  };
+  return (
+    <div className="circleJoinMemberList">
+      <div className="wantToJoinUser">
+        <div className="wantToJoinUserAvatar">
+          <img src={registerMember?.userPhotoURL} />
+        </div>
+        <div className="wantToJoinUserMessageBox">
+          <p className="wantToJoinUserName">{registerMember.userName}</p>
+          <div className="wantToJoinUserMessage">
+            <p className="wantToJoinUserMessageTextBox">
+              {registerMember.userMotivation}
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="repButtonBox">
+        <div className="wrapButton">
+          <Button
+            variant="outlined"
+            onClick={() => {
+              registerMemberCancel();
+            }}
+          >
+            <CancelIcon />
+            <p className="textOfButton">中止</p>
+          </Button>
+        </div>
+        <Button
+          variant="contained"
+          onClick={() => {
+            registerMemberAccept();
+          }}
+        >
+          <CheckCircleIcon />
+          <p className="textOfButton">同意</p>
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default function Members({ circleId }) {
+  const [memberList, setMemberList] = useState("");
+
+  //useEffect
+  useEffect(() => {
+    fetchCircleMemberData();
+  }, []);
+
+  //function
+  const fetchCircleMemberData = () => {
+    const query = db
+      .collection("circle")
+      .doc(circleId)
+      .collection("member")
+      .onSnapshot((querySnapshot) => {
+        const data = [];
+        querySnapshot.docs.map((doc) => {
+          let item = doc.data();
+          item.id = doc.id;
+          data.push(item);
+        });
+        setMemberList(data);
+      });
+    return query;
+  };
   return (
     <div className="memberPage">
-      <div className="wantToJoinUserBox">
-        <div className="circleTitleBox">
-          {<FiberManualRecordIcon fontSize="small" />}
-          <p>参加したいメンバーリスト</p>
-        </div>
-        <div className="wantToJoinUser">
-          <div className="wantToJoinUserAvatar">
-            <img src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" />
-          </div>
-          <div className="wantToJoinUserMessageBox">
-            <div className="wantToJoinUserMessage">
-              <p className="wantToJoinUserName">さくら</p>
-              <p className="wantToJoinUserMessageTextBox">
-                Helloooooooooooooooooooooooooooooooooooooooooooooooooooo
-              </p>
-            </div>
-
-            <div className="wantToJoinUserRep">
-              <div className="wantToJoinUserRepButton">
-                <DangerousIcon sx={{ color: red[800] }} />
-                <p>中止</p>
-              </div>
-              <div className="wantToJoinUserRepButton">
-                <CheckCircleIcon sx={{ color: green[800] }} />
-                <p>同意</p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="memberListBox">
-        <div className="circleTitleBox">
-          {<FiberManualRecordIcon fontSize="small" />}
-          <p>メンバーリスト</p>
-        </div>
-        <div className="memberList">
-          <div className="memberInfor">
-            <div className="memberAvatar">
-              <img src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" />
-            </div>
-          </div>
-          <div className="findMember"></div>
-        </div>
-      </div>
+      <MemberJoinListComponent
+        memberList={memberList}
+        circleId={circleId}
+      ></MemberJoinListComponent>
+      <MemberListComponent memberList={memberList}></MemberListComponent>
     </div>
   );
 }
