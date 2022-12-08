20f81a270bde904565162be644adc474f96fb020
VODAITRINHRIKKEI <trinhvd@rikkeisoft.com>
2022-12-08 14:00:28 +0900

    update

--- src/component/features/app/home/Home.js
+++ src/component/features/app/home/Home.js
@@ -30,6 +30,7 @@ import Slide from "@mui/material/Slide";
 import EditCircle from "./EditCircle";
 import Modal from "../../../ui/Modal";
 import ButtonComponent from "../../../ui/ButtonComponent";
+import "./EditCircle.css";
 import { arrayRemove } from "firebase/firestore";
 const Transition = React.forwardRef(function Transition(props, ref) {
   return <Slide direction="up" ref={ref} {...props} />;
--- src/component/features/app/home/event/EditEvent.js
+++ src/component/features/app/home/event/EditEvent.js
@@ -1,7 +1,71 @@
-import React from 'react'
-
-export default function EditEvent() {
+import React, { useState } from "react";
+import TitleText from "../../../../ui/TitleText";
+import { TextField } from "@mui/material";
+export default function EditEvent({ editEvent, parentCallback }) {
+  const [createNewEventInfor, setCreateNewEventInfor] = useState(editEvent);
+  const handleChange = (e) => {
+    setCreateNewEventInfor({
+      ...createNewEventInfor,
+      [e.target.name]: e.target.value,
+    });
+    sendNewEventInfor();
+  };
+  const sendNewEventInfor = () => {
+    parentCallback(createNewEventInfor);
+  };
   return (
-    <div>EditEvent</div>
-  )
+    <div className="createNewEvent">
+      <TitleText>イベント追加</TitleText>
+      <div className="creatNewEventInputBox">
+        <TextField
+          className="createNewEventTextField"
+          label="イベント名"
+          inputProps={{ maxLength: 25 }}
+          name="name"
+          value={createNewEventInfor.name}
+          onChange={handleChange}
+        ></TextField>
+        <TextField
+          className="createNewEventTextField"
+          label="時間"
+          inputProps={{ maxLength: 25 }}
+          name="time"
+          value={createNewEventInfor.time}
+          onChange={handleChange}
+        ></TextField>
+        <TextField
+          className="createNewEventTextField"
+          label="場所"
+          inputProps={{ maxLength: 25 }}
+          name="place"
+          value={createNewEventInfor.place}
+          onChange={handleChange}
+        ></TextField>
+        <TextField
+          className="createNewEventTextField"
+          label="料金"
+          inputProps={{ maxLength: 25 }}
+          name="money"
+          value={createNewEventInfor.money}
+          onChange={handleChange}
+        ></TextField>
+        <TextField
+          className="createNewEventTextField"
+          label="最大人数"
+          inputProps={{ maxLength: 25 }}
+          name="maxMembers"
+          value={createNewEventInfor.maxMembers}
+          onChange={handleChange}
+        ></TextField>
+        <TextField
+          className="createNewEventTextField"
+          label="イベント内容"
+          inputProps={{ maxLength: 25 }}
+          name="content"
+          value={createNewEventInfor.content}
+          onChange={handleChange}
+        ></TextField>
+      </div>
+    </div>
+  );
 }
