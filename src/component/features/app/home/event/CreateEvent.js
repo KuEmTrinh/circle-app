f39fd9a8c8436f9651c40689c6c4ec1424a89714
VODAITRINHRIKKEI <91253322+KuEmTrinh@users.noreply.github.com>
2023-01-17 13:10:24 +0900

    update

--- src/component/features/app/home/event/CreateEvent.js
+++ src/component/features/app/home/event/CreateEvent.js
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import TitleText from "../../../../ui/TitleText";
 import { FormControl } from "react-bootstrap";
 import { InputLabel, TextField } from "@mui/material";
 import "./CreateEvent.css";
-
+import FileUploadIcon from "@mui/icons-material/FileUpload";
+import imageCompression from "browser-image-compression";
 export default function CreateEvent(props) {
   const [createNewEventInfor, setCreateNewEventInfor] = useState({
     name: "",
@@ -23,10 +24,47 @@ export default function CreateEvent(props) {
   const sendNewEventInfor = () => {
     props.parentCallback(createNewEventInfor);
   };
+  const [percent, setPercent] = useState(0);
+  const [resultBox, setResultBox] = useState(false);
+  const [preview, setPreview] = useState();
+
+  useEffect(() => {
+    if (!props.file) {
+      setPreview(undefined);
+      return;
+    }
+
+    const objectUrl = URL.createObjectURL(props.file);
+    setPreview(objectUrl);
+
+    // free memory when ever this component is unmounted
+    return () => URL.revokeObjectURL(objectUrl);
+  }, [props.file]);
+  const handleChangeImage = async (event) => {
+    const options = {
+      maxSizeMB: 0.1,
+      maxWidthOrHeight: 400,
+      useWebWorker: true,
+    };
+    try {
+      const file = event.target.files[0];
+      const compressedFile = await imageCompression(file, options);
+      props.setFile(compressedFile);
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
     <div className="createNewEvent">
       <TitleText>イベント追加</TitleText>
       <div className="creatNewEventInputBox">
+        {preview ? (
+          <div className="circleImagePreview">
+            {props.file && <img src={preview} />}
+          </div>
+        ) : (
+          ""
+        )}
         <TextField
           className="createNewEventTextField"
           label="イベント名"
@@ -69,6 +107,22 @@ export default function CreateEvent(props) {
           name="content"
           onChange={handleChange}
         ></TextField>
+      </div>
+      <div className="inputBox">
+        <label for="accountSettingInput">
+          <div className="accountSettingInputUpload">
+            <div className="imageInputTitle">
+              <FileUploadIcon></FileUploadIcon>
+              <p>写真を選択</p>
+            </div>
+            <input
+              className="inputSetting"
+              onChange={handleChangeImage}
+              id="accountSettingInput"
+              type="file"
+            ></input>
+          </div>
+        </label>
       </div>
     </div>
   );
