bcc017e0ddeac6bdc32f5f60acfb4730dea20dbd
VODAITRINHRIKKEI <trinhvd@rikkeisoft.com>
2023-07-31 15:36:51 +0900

    update

--- src/component/features/app/home/chat/Chat.js
+++ src/component/features/app/home/chat/Chat.js
@@ -3,41 +3,144 @@ import { useParams } from "react-router-dom";
 import "./Chat.css";
 import TextField from "@mui/material/TextField";
 import SendIcon from "@mui/icons-material/Send";
-import { db } from "../../../../../app/firebase";
+import { ref, getDownloadURL, uploadBytesResumable } from "firebase/storage";
+import { db, storage } from "../../../../../app/firebase";
 import { firebase } from "../../../../../app/firebase";
 import { useSelector } from "react-redux";
+import AttachFileIcon from "@mui/icons-material/AttachFile";
+import imageCompression from "browser-image-compression";
 function ChatRoomInputBox({ circleId }) {
   const user = useSelector((state) => state.login.data);
   const [chatMessage, setChatMessage] = useState();
+  const fileInputRef = useRef(null); // Ref for the file input
+  const [file, setFile] = useState("");
+  const [preview, setPreview] = useState("");
+  useEffect(() => {
+    if (!file) {
+      return;
+    }
+
+    const objectUrl = URL.createObjectURL(file);
+    setPreview(objectUrl);
+
+    // free memory when ever this component is unmounted
+    return () => URL.revokeObjectURL(objectUrl);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [file]);
   //function
   const handleChangeMessage = async (e) => {
     setChatMessage(e.target.value);
   };
+  const handleAttachFile = () => {
+    fileInputRef.current.click(); // Trigger the file input when the AttachFileIcon is clicked
+  };
+  const handleUpdateImage = async () => {
+    try {
+      if (!file) {
+        const url =
+          "https://us.123rf.com/450wm/yehorlisnyi/yehorlisnyi2104/yehorlisnyi210400016/167492439-no-photo-or-blank-image-icon-loading-images-or-missing-image-mark-image-not-available-or-image-comin.jpg?ver=6";
+        return url;
+      } else {
+        const storageRef = ref(storage, `/files/${file.name}`);
+        const uploadTask = uploadBytesResumable(storageRef, file);
+        await uploadTask;
+        const url = await getDownloadURL(uploadTask.snapshot.ref);
+        return url;
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleFileChange = (e) => {
+    const selectedFile = e.target.files[0]; // Get the selected file
+    // Do whatever you want with the selected file here (e.g., upload to storage)
+    console.log("Selected file:", selectedFile);
+  };
   const sendMessage = async () => {
     let currentMessage = await chatMessage;
     await setChatMessage("");
-    await db.collection("circle").doc(circleId).collection("chat").add({
-      message: currentMessage,
-      userName: user.name,
-      userPhoto: user.photoURL,
-      userId: user.uid,
-      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
-    });
+    let responseUrl = "";
+    if (file) {
+      responseUrl = await handleUpdateImage();
+      await db.collection("circle").doc(circleId).collection("chat").add({
+        message: currentMessage,
+        userName: user.name,
+        userPhoto: user.photoURL,
+        imageUrl: responseUrl,
+        userId: user.uid,
+        createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      });
+      setFile("");
+      setPreview("");
+    } else {
+      await db.collection("circle").doc(circleId).collection("chat").add({
+        message: currentMessage,
+        userName: user.name,
+        userPhoto: user.photoURL,
+        userId: user.uid,
+        createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      });
+    }
+  };
+  const handleFileSelect = async (event) => {
+    const options = {
+      maxSizeMB: 0.1,
+      maxWidthOrHeight: 400,
+      useWebWorker: true,
+    };
+    try {
+      const file = event.target.files[0];
+      const compressedFile = await imageCompression(file, options);
+      setFile(compressedFile);
+    } catch (err) {
+      console.log(err);
+    }
   };
   return (
     <div className="center">
       <div className="chatInputBox">
+        <div className="chatImagePreview">
+          {preview ? <img src={preview} /> : ""}
+        </div>
         <TextField
           id="outlined-multiline-flexible"
           label="メッセージ"
           multiline
           maxRows={4}
-          className="chatInput"
           size="small"
+          fullWidth
           value={chatMessage}
           onChange={handleChangeMessage}
+          InputProps={{
+            endAdornment: (
+              <>
+                <input
+                  type="file"
+                  ref={fileInputRef}
+                  style={{ display: "none" }}
+                  onChange={handleFileSelect}
+                />
+                <AttachFileIcon
+                  fontSize="small"
+                  className="attachFileIcon"
+                  color="primary"
+                  sx={{ mr: 1 }}
+                  onClick={handleAttachFile}
+                  // Add any additional props for the AttachFileIcon here if needed
+                />
+                <SendIcon
+                  fontSize="small"
+                  color="primary"
+                  // className="sendIcon"
+                  onClick={sendMessage}
+                />
+              </>
+            ),
+          }}
         />
-        <div className="chatSendIcon center">
+
+        {/* <div className="chatSendIcon center">
           <div
             className="wrapChatSendIcon"
             onClick={() => {
@@ -50,15 +153,13 @@ function ChatRoomInputBox({ circleId }) {
               className="sendIcon"
             ></SendIcon>
           </div>
-        </div>
+        </div> */}
       </div>
     </div>
   );
 }
 
 function ChatMessage({ message, userId, messagePre }) {
-  // console.log(messagesPre?.userId)
-  console.log()
   let show = true;
   if (messagePre?.userId == message?.userId) {
     show = false;
@@ -91,6 +192,14 @@ function ChatMessage({ message, userId, messagePre }) {
                 <div className="messageContentLeftOnly">
                   <div className="messageText">{message.message}</div>
                 </div>
+
+                {message.imageUrl ? (
+                  <div className="messageImage">
+                    <img src={message.imageUrl} />
+                  </div>
+                ) : (
+                  ""
+                )}
               </div>
             </div>
           )}
@@ -161,7 +270,7 @@ function ChatMessages({ circleId }) {
             {messages.map((message, index) => {
               return (
                 <ChatMessage
-                  messagePre={messages[index-1]}
+                  messagePre={messages[index - 1]}
                   message={message}
                   userId={userId}
                   index={index}
